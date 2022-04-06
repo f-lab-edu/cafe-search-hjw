77dba2b3bb54bf91f4d95d1b601f5ec250430154
jungwon <jungwon1423@gmail.com>
2022-04-06 13:51:07 +0900

    fix: main

--- app/main.py
+++ app/main.py
@@ -3,11 +3,8 @@ from fastapi import FastAPI
 
 from routers import users
 from config import settings
-from models.models import Base
-from database import engine
 
 
-Base.metadata.create_all(engine)
 app = FastAPI()
 app.include_router(users.router, prefix="/users", tags=["users"])
 
