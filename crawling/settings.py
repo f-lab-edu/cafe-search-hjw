8344b94fffea8b62d1e5785e999f5b2a495140b0
jungwon <jungwon1423@gmail.com>
2022-04-20 00:16:16 +0900

    add: get cafe info and write csv

---
+++ crawling/settings.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+load_dotenv(str(BASE_DIR / ".env"))
+
+API_KEY = os.environ.get("API_KEY")
+
+GU_LIST = [
+        "강남구",
+        "강동구",
+        "강북구",
+        "강서구",
+        "관악구",
+        "광진구",
+        "구로구",
+        "금천구",
+        "노원구",
+        "도봉구",
+        "동대문구",
+        "동작구",
+        "마포구",
+        "서대문구",
+        "서초구",
+        "성동구",
+        "성북구",
+        "송파구",
+        "양천구",
+        "영등포구",
+        "용산구",
+        "은평구",
+        "종로구",
+        "중구",
+        "중랑구",
+    ]
